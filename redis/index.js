2e12f8c4f7428f20cafbecd54a4fbf6ad45232ed
Praveen Kumar <praveen9166050@gmail.com>
2024-09-12 23:09:44 +0530

    More on redis

--- redis/index.js
+++ redis/index.js
@@ -15,6 +15,30 @@ app.get('/', async (req, res) => {
   res.status(200).json(data);
 });
 
+app.get('/photos', async (req, res) => {
+  const albumId = req.query.albumId;
+  const cacheValue = await client.get(`photos?albumId=${albumId}`);
+  if (cacheValue) {
+    return res.status(200).json(JSON.parse(cacheValue));
+  }
+  const {data} = await axios.get("https://jsonplaceholder.typicode.com/photos", {
+    params: {albumId}
+  });
+  client.setex(`photos?albumId=${albumId}`, 3600, JSON.stringify(data));
+  res.status(200).json(data);
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const id = req.params.id;
+  const cacheValue = await client.get(`photos:${id}`);
+  if (cacheValue) {
+    return res.status(200).json(JSON.parse(cacheValue));
+  }
+  const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`);
+  client.setex(`photos:${id}`, 3600, JSON.stringify(data));
+  res.status(200).json(data);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}`);
